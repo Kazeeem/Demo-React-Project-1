b5511c6c03318ca893e96a65b4bf9f8e466acbc6
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-06-29 23:14:29 +0100

    Worked on navbar and theme switcher

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
+import {Header, Navbar} from "../components/index.js";
 
 const HomeLayout = () => {
     return (
         <>
-            <nav>
-                <span className="text-4xl text-primary">Comfy</span>
-            </nav>
-            <Outlet />
+            <Header />
+            <Navbar />
+
+            <section className="align-item py-20">
+                <Outlet />
+            </section>
         </>
     )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,26 @@
-const Register = () => {
+import { Form, Link } from "react-router-dom";
+import { FormInput, SubmitBtn } from "../components/index.js";
+
+const Login = () => {
     return (
-        <div>Register</div>
+        <section className="h-screen grid place-items-center">
+            <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
+                <h4 className="text-center text-3xl font-bold">Register</h4>
+                <FormInput type="text" label="username" name="username" />
+                <FormInput type="email" label="email" name="email" />
+                <FormInput type="password" label="password" name="password" />
+
+                <div className="mt-4">
+                    <SubmitBtn text="Register" />
+                </div>
+
+                <button type="button" className="btn btn-secondary btn-block">guest user</button>
+                <p className="text-center">
+                    Already a member?
+                    <Link to="/auth/login" className="ml-2 link link-hover link-primary capitalize">Login</Link>
+                </p>
+            </Form>
+        </section>
     )
 }
-export default Register
+export default Login
