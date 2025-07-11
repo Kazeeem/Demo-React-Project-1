039fd6eee35897f3bb4055b842fd62c692bf77be
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-07-11 02:27:33 +0100

    feat: Worked on the filters feature

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -5,17 +5,18 @@ import FormRange from "./FormRange.jsx";
 import FormCheckbox from "./FormCheckbox.jsx";
 
 const Filters = () => {
-    const {meta} = useLoaderData();
+    const {meta, params} = useLoaderData();
+    const {search,company,category,shipping,order,price} = params
 
     return <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-        <FormInput type="search" label="Search Product" name="search" size="input-sm" />
+        <FormInput type="search" label="Search Product" name="search" size="input-sm" defaultValue={search} />
 
-        <FormSelect label="Select Category" name="category" list={meta.categories} size="select-sm" />
-        <FormSelect label="Select Company" name="company" list={meta.companies} size="select-sm" />
-        <FormSelect label="Sort By" name="order" list={['a-z','z-a','high','low']} size="select-sm" />
+        <FormSelect label="Select Category" name="category" list={meta.categories} defaultValue={category} size="select-sm" />
+        <FormSelect label="Select Company" name="company" list={meta.companies} defaultValue={company} size="select-sm" />
+        <FormSelect label="Sort By" name="order" list={['a-z','z-a','high','low']} defaultValue={order} size="select-sm" />
 
-        <FormRange name="price" label="Select Price" size="range-sm" />
-        <FormCheckbox name="shipping" label="Free Shipping" size="checkbox-sm" />
+        <FormRange name="price" label="Select Price" size="range-sm" price={price} />
+        <FormCheckbox name="shipping" label="Free Shipping" size="checkbox-sm" defaultValue={shipping} />
 
         <button type="submit" className="btn btn-primary btn-sm">
             Search
