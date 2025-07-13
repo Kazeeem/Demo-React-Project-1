37104a96a580ea427f56b6b577f82097f1cb7dcd
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-07-13 08:06:05 +0100

    feat: Added functionality to allow guest user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import {Form, Link, redirect} from "react-router-dom";
+import {Form, Link, redirect, useNavigate} from "react-router-dom";
 import { FormInput, SubmitBtn } from "../components/index.js";
 import {customFetch} from "../utils/index.jsx";
 import {toast} from "react-toastify";
 import {loginUser} from "../features/user/userSlice.js";
+import {useDispatch} from "react-redux";
 
 export const action = (store) => async ({request}) => {
     const formData = await request.formData()
@@ -21,18 +22,37 @@ export const action = (store) => async ({request}) => {
 }
 
 const Login = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const loginAsGuestUser = async () => {
+        try {
+            const response = await customFetch.post('/auth/local', {
+                identifier: 'test@test.com',
+                password: 'secret'
+            })
+
+            dispatch(loginUser(response.data))
+            toast.success('Welcome guest user')
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+            toast.error('Guest user login error. Please try again')
+        }
+    }
+
     return (
         <section className="h-screen grid place-items-center">
             <Form method="post" className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4">
                 <h4 className="text-center text-3xl font-bold">Login</h4>
-                <FormInput type="email" label="email" name="identifier" defaultValue="test@test.com" />
-                <FormInput type="password" label="password" name="password" defaultValue="secret" />
+                <FormInput type="email" label="email" name="identifier" />
+                <FormInput type="password" label="password" name="password" />
 
                 <div className="mt-4">
                     <SubmitBtn text="Login" />
                 </div>
 
-                <button type="button" className="btn btn-secondary btn-block">guest user</button>
+                <button type="button" onClick={loginAsGuestUser} className="btn btn-secondary btn-block">Guest User</button>
                 <p className="text-center">
                     Not a member yet?
                     <Link to="/auth/register" className="ml-2 link link-hover link-primary capitalize">Register</Link>
