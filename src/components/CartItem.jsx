e54ff46eadb5e1f2c0f137142211d21f767635c7
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-07-13 05:26:24 +0100

    feat: Completed cart page features

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,18 @@
 import {formatPrice, generateAmountOptions} from "../utils/index.jsx";
+import {useDispatch} from "react-redux";
+import {editItem, removeItem} from "../features/cart/cartSlice.js";
 
 const CartItem = ({cartItem}) => {
     const {cartID, title, price, image, amount, company, productColor} = cartItem;
+    const dispatch = useDispatch();
+
+    const removeItemFromTheCart = () => {
+        dispatch(removeItem({cartID}));
+    }
+
+    const handleAmount = (e) => {
+        dispatch(editItem({cartID, amount:parseInt(e.target.value)}));
+    }
 
     return <article key={cartID} className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0">
         <img src={image} alt={title} className="h-24 w-24 rounded-lg sm:h-32 sm:w-32 object-cover" />
@@ -14,16 +25,16 @@ const CartItem = ({cartItem}) => {
             </p>
         </div>
 
-        <div className="sm:ml-24">
+        <div className="sm:ml-12">
             <div className="form-control max-w-xs">
                 <label htmlFor="amount" className="label p-0">
                     <span className="label-text">Amount</span>
                 </label>
-                <select name="amount" id="amount" className="mt-2 select select-base select-bordered select-xs">
+                <select onChange={handleAmount} name="amount" id="amount" value={amount} className="mt-2 select select-base select-bordered select-xs">
                     {generateAmountOptions(amount + 5)}
                 </select>
             </div>
-            <button className="mt-2 link link-primary link-hover text-xs">Remove</button>
+            <button onClick={removeItemFromTheCart} className="mt-2 link link-primary link-hover text-xs">Remove</button>
         </div>
 
         <p className="font-medium sm:ml-auto">{formatPrice(price)}</p>
