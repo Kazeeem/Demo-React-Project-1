38619af83a6d6936de543d326f3cb2b79d5031da
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-07-13 08:21:49 +0100

    feat: Working on the checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,33 @@
+import {useSelector} from "react-redux";
+import {SectionTitle} from "../components/index.js";
+import CheckoutForm from "../components/CheckoutForm.jsx";
+import CartTotals from "../components/CartTotals.jsx";
+import {toast} from "react-toastify";
+import {redirect} from "react-router-dom";
+
+export const loader = (store) => () => {
+    const user = store.getState().userState.user
+    if (!user) {
+        toast.warn('You must be logged in');
+        return redirect('/auth/login')
+    }
+
+    return null;
+}
+
 const Checkout = () => {
-    return (
-        <div>Checkout</div>
-    )
+    const cartTotal = useSelector((state) => state.cartState.cartTotal);
+
+    if (cartTotal === 0) {
+        return <SectionTitle text="Your cart is empty" />
+    }
+
+    return <>
+        <SectionTitle text="Place your order" />
+        <div className="mt-8 grid gap-8 md:grid-cols-2 start">
+            <CheckoutForm />
+            <CartTotals />
+        </div>
+    </>
 }
 export default Checkout
